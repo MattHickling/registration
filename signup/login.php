8f9183a2dd242c4008b87ace9d7d76c49223d2fb
Matt Hickling <m.hickling@hotmail.co.uk>
2024-06-18 20:23:22 +0100

    forget started

--- signup/login.php
+++ signup/login.php
@@ -50,9 +50,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 <label class="form-label">Password</label>
                 <input type="password" class="form-control" name="password" required>
             </div>
-            <button type="submit" class="btn btn-outline-primary w-100">Login</button>
+            <button type="submit" class="btn btn-primary w-100">Login</button>
         </form>
     </div>
+    <div class="login">
+        <a href="forget.php" class="btn btn-succes">Forgotten password</a>
+    </div> 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
